46f2716b749f2256ab3f754ba3dbe22de60ad728
jesvia <jesvia.rofl@gmail.com>
2025-02-02 13:00:18 +0530

    created update-user-listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,6 @@
+import e from "express";
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
@@ -16,6 +18,21 @@ export const deleteListing = async (req, res, next) => {
     try {
         await Listing.findByIdAndDelete(req.params.id);
         return res.status(200).json("Listing has been deleted");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if(!listing) return next(errorHandler(404, 'Listing not found'));
+    if (req.user.id !== listing.userRef) return next(errorHandler(401, 'You can update only your listing'));
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            {new: true});
+            res.status(200).json(updatedListing);
     } catch (error) {
         next(error);
     }
